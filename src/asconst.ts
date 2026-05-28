461c2a6d7ae713bcee38288d179354303b77337c
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-28 23:31:32 +0600

    added

--- src/asconst.ts
+++ src/asconst.ts
@@ -56,11 +56,11 @@ UserRoles['Admin']  >>> 'ADMIN'
 }
 */
 
-const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
-  if (role === UserRoles.Admin || role === UserRoles.Editor) {
-    return true;
-  } else return false;
-};
+// const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
+//   if (role === UserRoles.Admin || role === UserRoles.Editor) {
+//     return true;
+//   } else return false;
+// };
 
 const isEditPermissable = canEdit(UserRoles.Admin);
 console.log(isEditPermissable);
