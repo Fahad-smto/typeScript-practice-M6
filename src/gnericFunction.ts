b7a8b5a60c2156677f799eb91a305e71c852f166
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-28 23:27:51 +0600

    added

--- src/gnericFunction.ts
+++ src/gnericFunction.ts
@@ -21,10 +21,10 @@ const arrObj = createArrrayWithGeneric({
 
 // tuple
 
-const createArrayWithTuple = (param1: string, param2: string) => [
-  param1,
-  param2,
-];
+// const createArrayWithTuple = (param1: string, param2: string) => [
+//   param1,
+//   param2,
+// ];
 
 const createArrayTupleWithGeneric = <X, Y>(param1: X, param2: Y) => [
   param1,
